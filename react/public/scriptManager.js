abb5705cc04016475f7b9c38002898d238926f7c
gabrielmikhail <145404255+gabrielmikhail@users.noreply.github.com>
2024-11-11 15:19:47 -0600

    added js file for manager view

---
+++ react/public/scriptManager.js
@@ -0,0 +1,8 @@
+//Translation Functionality
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        { pageLanguage: "en" },
+        "googleTranslateElement"
+    )
+}
+
