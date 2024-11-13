9040b6f15dfdf161244742b85e4f2be0e2439a10
gabrielmikhail <145404255+gabrielmikhail@users.noreply.github.com>
2024-11-13 09:49:54 -0600

    added in functional tabs for manager view

--- react/public/scriptManager.js
+++ react/public/scriptManager.js
@@ -6,3 +6,26 @@ function googleTranslateElementInit() {
     )
 }
 
+
+function setupTabs() {
+    const tabs = document.querySelectorAll('.tabLinks');
+    const contents = document.querySelectorAll('.tabContent');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+           
+            tabs.forEach(t => t.classList.remove('active'));
+            contents.forEach(content => content.style.display = 'none');
+
+            
+            tab.classList.add('active');
+            contents[index].style.display = 'block';
+        });
+    });
+
+
+    tabs[0].classList.add('active');
+    contents[0].style.display = 'block';
+}
+
+window.onload = setupTabs;
